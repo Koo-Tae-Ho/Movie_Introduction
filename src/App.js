b7b81fcc839cb45f0664a1c98beb606f85e5a69d
구태호 <gth0908@naver.com>
2023-10-10 12:57:31 +0900

    gh-pages 테스트2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />}></Route>
+                <Route
+                    path={`${process.env.PUBLIC_URL}/`}
+                    element={<Home />}
+                ></Route>
                 <Route path="/movie/:id" element={<Detail />}></Route>
             </Routes>
         </BrowserRouter>
